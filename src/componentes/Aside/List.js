934ac95c0bcd7c9740ccf3d4ed63e6dceace6232
juanacopello <61588695+juanacopello@users.noreply.github.com>
2021-01-17 02:30:19 -0300

    componente lista, estilos aside

---
+++ src/componentes/Aside/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const List = () => {
+  return(
+    <ul className="list">
+    <li>Categoria 1</li>
+    <li>Categoria 2</li>
+    <li>Categoria 3</li>
+    <li>Categoria 4</li>
+    <li>Categoria 5</li>
+  </ul>
+  )
+}
+
+export default List
--- src/componentes/Aside/index.js
+++ src/componentes/Aside/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import List from './List.js'
 
 const Aside = () => {
   return(
@@ -6,13 +7,7 @@ const Aside = () => {
     <h2>Notebook</h2>
     <h4>30.096 resultados</h4>
     <h3>Categorias</h3>
-    <ul className="list">
-      <li>Categoria 1</li>
-      <li>Categoria 2</li>
-      <li>Categoria 3</li>
-      <li>Categoria 4</li>
-      <li>Categoria 5</li>
-    </ul>
+    <List/>
   </aside>
   )
 };
